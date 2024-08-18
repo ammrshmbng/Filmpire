c241e614e0ae2a0ad307f985f0dab8b97c88efba
ammrshmbng <ammarmare01@gmail.com>
2024-08-18 21:22:02 +0700

    create components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,20 @@
+import { CssBaseline } from '@mui/material';
+import { Route, Routes } from 'react-router-dom';
+import { Movies, Actors, MovieInformation, Navbar, Profile } from './index';
 
 function App() {
   return (
-    <h1>Filmpire</h1>
+    <div>
+      <CssBaseline />
+      <main>
+        <Routes>
+          <Route exact path="/movies/:id" element={<h1>Movie Information</h1>} />
+          <Route exact path="/actors/:id" element={<h1>Actors</h1>} />
+          <Route exact path="/" element={<h1>Movies</h1>} />
+          <Route exact path="/profile/:id" element={<h1>Profile</h1>} />
+        </Routes>
+      </main>
+    </div>
   );
 }
 
