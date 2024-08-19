650ecf11035d8437d6cfcb1135f261543e2ef5d6
ammrshmbng <ammarmare01@gmail.com>
2024-08-19 10:33:46 +0700

    adding some style in NavBar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import { AppBar, IconButton, Toolbar, Drawer, Button, Avatar } from '@mui/material';
+import { Menu, AccountCircle, Brightness4, Brightness7 } from '@mui/icons-material';
+import { Link } from 'react-router-dom';
+
+import useStyles from './styles';
 
 function NavBar() {
+  const classes = useStyles();
   return (
-    <div>NavBar</div>
+    <AppBar position="fixed">
+      <Toolbar className={classes.toolbar} />
+    </AppBar>
   );
 }
 
