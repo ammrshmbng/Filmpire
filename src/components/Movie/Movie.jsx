2ff9a71b1dbd6f437d79e6fff9cbe06163c338f6
ammrshmbng <ammarmare01@gmail.com>
2024-08-21 12:02:04 +0700

    styling card

--- src/components/Movie/Movie.jsx
+++ src/components/Movie/Movie.jsx
@@ -10,12 +10,14 @@ function Movie({ movie, i }) {
     <Grid item xs={12} sm={6} md={4} lg={3} xl={2} className={classes.movie}>
       <Grow in key={i} timeout={(i + 1) * 250}>
         <Link className={classes.links} to={`/movie/${movie.id}`}>
-          {movie.poster_path
-            ? <img className={classes.image} src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} />
-            : <img className={classes.image} src="https://www.fillmurray.com/200/300" alt={movie.title} />}
+          <img
+            src={movie.poster_path ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}` : 'https://www.fillmurray.com/200/300'}
+            alt={movie.title}
+            className={classes.image}
+          />
+          <Typography className={classes.title} variant="h5">{movie.title}</Typography>
         </Link>
       </Grow>
-      <Typography className={classes.title} variant="h5">{movie.title}</Typography>
     </Grid>
   );
 }
