96d5ad03a2eeb0675105527d43c091bc0abaa1a9
ammrshmbng <ammarmare01@gmail.com>
2024-08-22 09:44:55 +0700

    still bug, data not appear

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -3,14 +3,18 @@ import { TextField, InputAdornment } from '@mui/material';
 import { Search as SearchIcon } from '@mui/icons-material';
 import { useDispatch, useSelector } from 'react-redux';
 import { useLocation } from 'react-router-dom';
+import { searchMovie } from '../../features/currentGenreOrCategory';
 
 import useStyles from './styles';
 
 function Search() {
   const classes = useStyles();
   const [query, setQuery] = useState('');
-  const handleKeyPress = () => {
-
+  const dispatch = useDispatch();
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      dispatch(searchMovie(query));
+    }
   };
 
   return (
@@ -18,7 +22,7 @@ function Search() {
       className={classes.searchContainer}
     >
       <TextField
-        onKeyPress={handleKeyPress}
+        onKeyDown={handleKeyPress}
         value={query}
         onChange={(e) => setQuery(e.target.value)}
         variant="standard"
