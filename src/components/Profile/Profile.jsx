1cd32cda25046eff2a7b2839df60637bec461581
ammrshmbng <ammarmare01@gmail.com>
2024-08-22 15:02:01 +0700

    logout functionality

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,15 +1,35 @@
+import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
+import { Box, Button, Typography } from '@mui/material';
+import { ExitToApp } from '@mui/icons-material';
 import { userSelector } from '../../features/auth';
 
 function Profile() {
   const { user } = useSelector(userSelector);
-  console.log(user);
+
+  const favoriteMovies = [];
+  const logout = () => {
+    localStorage.clear();
+
+    window.location.href = '/';
+  };
 
   return (
-    <div>
-      Profile {user.username}
-
-    </div>
+    <Box>
+      <Box display="flex" justifyContent="space-between">
+        <Typography variant="h4" gutterBottom>My Profile</Typography>
+        <Button color="inherit" onClick={logout}>
+          Logout &nbsp; <ExitToApp />
+        </Button>
+      </Box>
+      {!favoriteMovies.length
+        ? <Typography variant="h5">Add favourite or watchlist same movies to see them here!</Typography>
+        : (
+          <Box>
+            Favorite Movies
+          </Box>
+        )}
+    </Box>
   );
 }
 
