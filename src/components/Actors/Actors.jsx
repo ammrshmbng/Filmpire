55ba105af217b810b3660fa552ceb2cddcf2486d
ammrshmbng <ammarmare01@gmail.com>
2024-08-24 18:47:47 +0700

    log the id actor

--- src/components/Actors/Actors.jsx
+++ src/components/Actors/Actors.jsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import { useState } from 'react';
+import { Box, Button, CircularProgress, Grid, Typography } from '@mui/material';
+import { useNavigate, useParams } from 'react-router-dom';
+import { ArrowBack } from '@mui/icons-material';
 
 function Actors() {
+  const { id } = useParams();
+
   return (
-    <div>Actors</div>
+    <div>Actors {id}</div>
   );
 }
 
