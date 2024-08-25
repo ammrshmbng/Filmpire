7eec95e09c35c14555d9cca113978596c760adc2
ammrshmbng <ammarmare01@gmail.com>
2024-08-25 19:17:57 +0700

    fix not display after auth

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,9 +14,10 @@ function App() {
       <main className={classes.content}>
         <div className={classes.toolbar} />
         <Routes>
+          <Route exact path="/" element={<Movies />} />
+          <Route exact path="/approved" element={<Movies />} />
           <Route exact path="/movie/:id" element={<MovieInformation />} />
           <Route exact path="/actors/:id" element={<Actors />} />
-          <Route exact path="/" element={<Movies />} />
           <Route exact path="/profile/:id" element={<Profile />} />
         </Routes>
       </main>
